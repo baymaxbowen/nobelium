fc80faaf6e4f39b2686a77fa541dfb6a780337b4
bowen <yibowen@dd.com>
2021-04-28 14:27:09 +0800

    feat: 增加图标

--- pages/_document.js
+++ pages/_document.js
@@ -83,8 +83,12 @@ class MyDocument extends Document {
             </>
           )}
 
-          <link rel="icon" href="/favicon.ico" />
+          {/* <link rel="icon" href="/favicon.ico" /> */}
           <link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+          <link rel="manifest" href="/site.webmanifest"></link>
           {BLOG.analytics && BLOG.analytics.provider === 'ackee' && (
             <script
               async
